0e9aa822ed267a2ca42cb8daabd3cac3f28f3a45
StoianAlinBogdan <alin-bogdan.stoian@student.tuiasi.ro>
2022-02-11 13:43:26 +0200

    pregatire pentru statistici - doar distributii uniforme, deocamdata

--- LicentaPractice.py
+++ LicentaPractice.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[11]:
 
 
 from qiskit import Aer, QuantumCircuit
 import matplotlib.pyplot as plt
-
+import pandas as pd
+import math
+from statsmodels.stats.weightstats import ztest as ztest
 
 get_ipython().run_line_magic('matplotlib', 'inline')
 plt.rcParams['figure.figsize'] = [10, 5]
@@ -27,17 +29,17 @@ qc.draw(output='mpl')
 sim = Aer.get_backend('aer_simulator')
 result = sim.run(qc, shots=100000).result()
 counts = result.get_counts()
-len(counts)
+counts
 
 
 # In[4]:
 
 
-plt.bar([int(x, 2) for x in counts.keys()], counts.values(), width=5)
+plt.bar([int(x, 2) for x in counts.keys()], counts.values(), width=1)
 plt.show()
 
 
-# In[18]:
+# In[5]:
 
 
 #from qiskit.circuit.library import UniformDistribution
@@ -46,12 +48,45 @@ qc2 = UniformDistribution(8)
 qc2.measure_all()
 result2 = sim.run(qc2.decompose(), shots=100000, memory=True).result()
 counts2 = result2.get_counts()
-plt.bar([int(x, 2) for x in counts2.keys()], counts2.values(), width=5)
+plt.bar([int(x, 2) for x in counts2.keys()], counts2.values(), width=1)
 plt.show()
 
 
-# In[19]:
+# In[6]:
 
 
-get_ipython().system('jupyter nbconvert --to script LicentaPractice.ipynb')
+#df1_data = {"number": [numere], "counts": [counts]}
+df1_data = {"number": [int(x, 2) for x in counts.keys()], "counts": [x for x in counts.values()]}
+df1 = pd.DataFrame.from_dict(df1_data)
+df2_data = {"number": [int(x, 2) for x in counts2.keys()], "counts": [x for x in counts2.values()]}
+df2 = pd.DataFrame.from_dict(df2_data)
+plt.bar(df1['number'], df1['counts'], width=1)
+plt.show()
+plt.bar(df2['number'], df2['counts'], width=1)
+plt.show()
+
+
+# In[7]:
+
+
+# asta normal ca nu merge, sunt distributii uniforme, pana la urma - ar merge daca as normaliza count-urile sau ceva
+z_table = 1 - 0.9675 # sau -1.96, e two-tailed
+#z_calc = (df1['counts'].mean() - df2['counts'].mean()) / (math.sqrt(df1.var()['counts'] / len(df1['counts']) + df2.var()['counts']/ len(df2['counts'])))
+df1['mult'] = df1['number'] * df1['counts']
+df2['mult'] = df2['number'] * df2['counts']
+z_calc = (df1['mult'].mean() - df2['mult'].mean()) / (math.sqrt(df1.var()['mult'] / len(df1['mult']) + df2.var()['mult']/ len(df2['mult'])))
+df1
+
+
+# In[13]:
+
+
+# si asa s-ar face cu metoda gata implementata, daca ar fi distributie normala
+ztest(df1['mult'], df2['mult'], value=0)
+
+
+# In[ ]:
+
+
+
 
