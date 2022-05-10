33e8f93e86b27eea2f6cab8f772afa4c0775e0b1
StoianAlinBogdan <alin-bogdan.stoian@student.tuiasi.ro>
2022-05-10 17:12:03 +0300

    PRNGs Cap. 1, Cap. 2 start

--- KISS.py
+++ KISS.py
@@ -1,8 +1,5 @@
 jsr = 123456789 # nu-s de acord, dar marsaglia e bazat
 jcong = 380116160 # cine stie cum dracu a facut rost de numarul asta
-
-#znew = 36969 * (362436069 and 65535) + (362436069 >> 16)
-#wnew = 18000 * (521288629 and 65535) + (521288629 >> 16)
 z = 362436069
 w = 521288629
 
@@ -24,18 +21,18 @@ def CONG():
 
 def znew():
     global z
-    z = (36969 * (z and 65536) + (z >> 16) )% (2 ** 32)
+    z = (36969 * (z and 65536) + (z >> 16) )% (2 ** 32) # sau 16?
     return z
     
 
 def wnew():
     global w 
-    w = (18000 * (w and 65535) + (w >> 16) )% (2 ** 32)
+    w = (18000 * (w and 65535) + (w >> 16) )% (2 ** 32) # sau 16?
     return w
     
 
 def MWC():
-    return ((znew() << 16) + wnew()) % (2 ** 32)
+    return ((znew() << 16) + wnew()) % (2 ** 32) # sau 16?
     
 
 def KISS():
