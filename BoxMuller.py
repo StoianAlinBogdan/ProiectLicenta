544d99773c2b167b7c6446ef201b838ace29a73f
StoianAlinBogdan <alin-bogdan.stoian@student.tuiasi.ro>
2022-05-22 00:03:25 +0300

    Box-Muller done, gave up on Kologomorov

--- BoxMuller.py
+++ BoxMuller.py
@@ -3,18 +3,21 @@ import math
 import matplotlib.pyplot as plt
 
 def BoxMuller(u1, u2):
-    z = np.sqrt(-2 * np.log(u1)) * np.cos(2 * math.pi * u2)
-    return z
+    z1 = np.sqrt(-2 * np.log(u1)) * np.cos(2 * math.pi * u2)
+    z2 = np.sqrt(-2 * np.log(u1)) * np.sin(2 * math.pi * u2)
+    return (z1, z2)
     
 if __name__ == "__main__":
-    u1 = np.random.rand(800000) * 10
-    plt.subplot(3,1,1)
+    u1 = np.random.rand(800000)
+    plt.subplot(4,1,1)
     plt.hist(u1)
-    u2 = np.random.rand(800000) * 10
-    plt.subplot(3,1,2)
+    u2 = np.random.rand(800000)
+    plt.subplot(4,1,2)
     plt.hist(u2)
-    z = BoxMuller(u1, u2)
-    plt.subplot(3,1,3)
-    plt.hist(z)
+    z1, z2 = BoxMuller(u1, u2)
+    plt.subplot(4,1,3)
+    plt.hist(z1)
+    plt.subplot(4,1,4)
+    plt.hist(z2)
     plt.show()
     
