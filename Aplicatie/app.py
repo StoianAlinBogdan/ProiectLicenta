b8e2b5ea0f771192e49283f69497d29d7503d818
StoianAlinBogdan <alin-bogdan.stoian@student.tuiasi.ro>
2022-06-08 00:59:14 +0300

    GUI App plotting progress

--- Aplicatie/app.py
+++ Aplicatie/app.py
@@ -8,7 +8,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 def draw_figure(canvas, figure):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
     figure_canvas_agg.draw()
-    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
+    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=0)
     return figure_canvas_agg
 
 
@@ -21,26 +21,32 @@ if __name__ == "__main__":
 
     AppFont = 'Any 16'
 
-    menu_def = [['&Mode',  ['Simulated', 'Real']], ['&Help']]
+    qrngs = QRNG()
 
-    left_part = [ 
-        sg.Listbox(values=['test1', 'test2', 'test3'], key='-LISTBOX-', size=(30,6), expand_y=True)
+    left_part = [
+        [
+            sg.Listbox(values=[x for x in qrngs.QRNGs.keys()], key='-LISTBOX-', size=(30,None), expand_y=True, enable_events=True)
+        ]
     ]
     right_part = [
-        sg.Canvas(key='-CANVAS-')
+        [
+            sg.Canvas(key='-CANVAS-')
+        ],
+        [
+            sg.Button('Generate', )
+        ]
     ]
+
+    menu_def = [['&Mode',  ['Simulated', 'Real']], ['&Help']]
     layout = [
         [sg.Menu(menu_def, tearoff=False, key='-MENU-', font=AppFont)],
-        [sg.Column([left_part], expand_y=True), sg.VSeparator(), sg.Column([right_part])]
+        [sg.Column(left_part, expand_y=True), sg.VSeparator(), sg.Column(right_part, element_justification='center')]
     ]
 
+    '''
 
-    qrngs = QRNG()
-    nums = qrngs.run_Hadamard_1bit(10000)
-    unique_nums = list(set(nums))
-    my_counts1 = {x: nums.count(x) for x in unique_nums}
+    '''
     fig = plt.figure()
-    plt.bar(my_counts1.keys(), my_counts1.values(), width=1)
 
     _VARS['window'] = sg.Window(
                         'TheWindow',
@@ -50,11 +56,25 @@ if __name__ == "__main__":
 
     )
     draw_figure(_VARS['window']['-CANVAS-'].TKCanvas, fig)
-
+    _VARS['window']['-LISTBOX-'].update(set_to_index=[0])
     while True:
-        event, values = _VARS['window'].read(timeout=200)
+        event, values = _VARS['window'].read(timeout=500)
         if event == sg.WIN_CLOSED or event == 'Exit':
             break
+        if event == '-LISTBOX-':
+            generator_string = values['-LISTBOX-'][0]
+            sg.eprint(generator_string)
+        if event == 'Generate':
+            if values['-LISTBOX-'][0] == 'Hadamard1Bit':
+                nums = qrngs.run_Hadamard_1bit(10000)
+                unique_nums = list(set(nums))
+                my_counts1 = {x: nums.count(x) for x in unique_nums}
+                plt.close(fig)
+                fig = plt.figure()
+                plt.bar(my_counts1.keys(), my_counts1.values(), width=1) 
+                draw_figure(_VARS['window']['-CANVAS-'].TKCanvas, fig)
+
+
     _VARS['window'].close()
 
 
