2701b5b8d68bc322faa0e918651625767befc93d
StoianAlinBogdan <alin-bogdan.stoian@student.tuiasi.ro>
2022-06-09 00:10:59 +0300

    App progress

--- Aplicatie/Generators.py
+++ Aplicatie/Generators.py
@@ -5,6 +5,7 @@ import math
 
 class QRNG:
     def __init__(self):
+        self.sim = Aer.get_backend('aer_simulator')
         self.QC_Hadamard_1bit = QuantumCircuit(1)
         self.QC_Hadamard_1bit.h(0)
         self.QC_RY_1bit = QuantumCircuit(1)
@@ -14,14 +15,13 @@ class QRNG:
         self.QC_RY_8bit = QuantumCircuit(8)
         self.QC_RY_8bit.ry(math.pi/2, range(0, 8))
         self.QC_Uniform_1bit = UniformDistribution(1)
+        self.QC_Uniform_1bit = transpile(self.QC_Uniform_1bit, self.sim, optimization_level=3)
         self.QC_Uniform_8bit = UniformDistribution(8)
+        self.QC_Uniform_8bit = transpile(self.QC_Uniform_8bit, self.sim, optimization_level=3)
         self.QRNGs = [self.QC_Hadamard_1bit, self.QC_Hadamard_8bit, self.QC_RY_1bit, self.QC_RY_8bit, self.QC_Uniform_1bit, self.QC_Uniform_8bit]
         for i in range(len(self.QRNGs)):
             self.QRNGs[i].measure_all()
-        self.sim = Aer.get_backend('aer_simulator')
-        for i in range(len(self.QRNGs)):
-            self.QRNGs[i] = transpile(self.QRNGs[i], self.sim, optimization_level=3)
-        
+
         self.QRNGs = {
             'Hadamard1Bit': self.QC_Hadamard_1bit,
             'Hadamard8Bit': self.QC_Hadamard_8bit,
@@ -51,3 +51,42 @@ class QRNG:
         memory = result.get_memory()
         numbers = self.concatenate_bits(memory)
         return numbers
+
+    def run_Hadamard_8bit(self, amount):
+        qc = self.QRNGs['Hadamard8Bit']
+        result = self.sim.run(qc, shots=amount, memory=True).result()
+        memory = result.get_memory()
+        for i in range(len(memory)):
+            memory[i] = int(memory[i], 2)
+        return memory   
+    
+    def run_RY_1bit(self, amount):
+        qc = self.QRNGs['RY1Bit']
+        result = self.sim.run(qc, shots=8*amount, memory=True).result()
+        memory = result.get_memory()
+        numbers = self.concatenate_bits(memory)
+        return numbers
+    
+    def run_RY_8bit(self, amount):
+        qc = self.QRNGs['RY8Bit']
+        result = self.sim.run(qc, shots=amount, memory=True).result()
+        memory = result.get_memory()
+        for i in range(len(memory)):
+            memory[i] = int(memory[i], 2) 
+        return memory    
+
+    def run_Uniform_1bit(self, amount):
+        qc = self.QRNGs['Uniform1Bit']
+        qc.draw(output='mpl', filename='./testImg.png')
+        result = self.sim.run(qc, shots=8*amount, memory=True).result()
+        memory = result.get_memory()
+        numbers = self.concatenate_bits(memory)
+        return numbers
+
+    def run_Uniform_8bit(self, amount):
+        qc = self.QRNGs['Uniform8Bit']
+        result = self.sim.run(qc, shots=amount, memory=True).result()
+        memory = result.get_memory()
+        for i in range(len(memory)):
+            memory[i] = int(memory[i], 2)
+        return memory  
--- Aplicatie/app.py
+++ Aplicatie/app.py
@@ -1,39 +1,74 @@
+from enum import unique
 import PySimpleGUI as sg
-from Generators import QRNG
+import Generators as gen
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+_VARS = {
+    'window': False,
+    'fig_agg': False,
+    'plt_fig': False
+    }
 
+function_map = {
+    'Hadamard1Bit': 'run_Hadamard_1bit',
+    'Hadamard8Bit': 'run_Hadamard_8bit',
+    'RY1Bit': 'run_RY_1bit',
+    'RY8Bit': 'run_RY_8bit',
+    'Uniform1Bit': 'run_Uniform_1bit',
+    'Uniform8Bit': 'run_Uniform_8bit'
+}
 
-def draw_figure(canvas, figure):
+def draw_figure(canvas, figure) -> None:
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
     figure_canvas_agg.draw()
     figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=0)
     return figure_canvas_agg
 
 
+def draw_chart(data: list) -> None:
+    _VARS['plt_fig'] = plt.figure()
+    unique_nums = list(set(data))
+    counts = {x: data.count(x) for x in unique_nums}
+    plt.bar(counts.keys(), counts.values(), width=1)
+    plt.title('Distribution of the numbers')
+    plt.xlabel('Number')
+    plt.ylabel('Frequency of number')
+    _VARS['fig_agg'] = draw_figure(_VARS['window']['-CANVAS-'].TKCanvas, _VARS['plt_fig'])
+
+
+def update_chart(data: list) -> None:
+    _VARS['fig_agg'].get_tk_widget().forget()
+    plt.clf()
+    unique_nums = list(set(data))
+    counts = {x: data.count(x) for x in unique_nums}    
+    plt.bar(counts.keys(), counts.values(), width=1)
+    plt.title('Distribution of the numbers')
+    plt.xlabel('Number')
+    plt.ylabel('Frequency of number')
+    _VARS['fig_agg'] = draw_figure(_VARS['window']['-CANVAS-'].TKCanvas, _VARS['plt_fig'])   
+    
+
 
 if __name__ == "__main__":
-
-    _VARS = {'window': False}
     sg.theme('LightGrey')
     sg.set_options(element_padding=(1, 1))
 
     AppFont = 'Any 16'
 
-    qrngs = QRNG()
+    qrngs = gen.QRNG()
 
     left_part = [
         [
-            sg.Listbox(values=[x for x in qrngs.QRNGs.keys()], key='-LISTBOX-', size=(30,None), expand_y=True, enable_events=True)
+            sg.Listbox(values=[x for x in qrngs.QRNGs.keys()], key='-LISTBOX-', size=(30,None), expand_x=True, expand_y=True, enable_events=True)
         ]
     ]
     right_part = [
         [
-            sg.Canvas(key='-CANVAS-')
+            sg.Canvas(key='-CANVAS-', size=(650,550))
         ],
         [
-            sg.Button('Generate', )
+            sg.Text("Amount of numbers: "), sg.Input(key='-NUMS-'), sg.Button('Generate', disabled=False, key='-GENERATE_BUTTON-')
         ]
     ]
 
@@ -43,10 +78,6 @@ if __name__ == "__main__":
         [sg.Column(left_part, expand_y=True), sg.VSeparator(), sg.Column(right_part, element_justification='center')]
     ]
 
-    '''
-
-    '''
-    fig = plt.figure()
 
     _VARS['window'] = sg.Window(
                         'TheWindow',
@@ -55,24 +86,25 @@ if __name__ == "__main__":
                         resizable=True,
 
     )
-    draw_figure(_VARS['window']['-CANVAS-'].TKCanvas, fig)
     _VARS['window']['-LISTBOX-'].update(set_to_index=[0])
+    generator_string = 'Hadamard1Bit'
     while True:
         event, values = _VARS['window'].read(timeout=500)
+        if values['-NUMS-'] != '':
+            _VARS['window']['-GENERATE_BUTTON-'].update(disabled=False)
+        else:
+            _VARS['window']['-GENERATE_BUTTON-'].update(disabled=True)         
         if event == sg.WIN_CLOSED or event == 'Exit':
             break
         if event == '-LISTBOX-':
             generator_string = values['-LISTBOX-'][0]
-            sg.eprint(generator_string)
-        if event == 'Generate':
-            if values['-LISTBOX-'][0] == 'Hadamard1Bit':
-                nums = qrngs.run_Hadamard_1bit(10000)
-                unique_nums = list(set(nums))
-                my_counts1 = {x: nums.count(x) for x in unique_nums}
-                plt.close(fig)
-                fig = plt.figure()
-                plt.bar(my_counts1.keys(), my_counts1.values(), width=1) 
-                draw_figure(_VARS['window']['-CANVAS-'].TKCanvas, fig)
+        if event == '-GENERATE_BUTTON-':
+            RNG = getattr(qrngs, function_map[generator_string])
+            nums = RNG(int(values['-NUMS-']))
+            if _VARS['plt_fig'] == False:
+                draw_chart(nums)
+            else:
+                update_chart(nums)
 
 
     _VARS['window'].close()
