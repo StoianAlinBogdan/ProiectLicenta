91243740a111a507c3a5780a837d6a69d4bfbdee
StoianAlinBogdan <alin-bogdan.stoian@student.tuiasi.ro>
2022-06-23 01:58:15 +0300

    Added tests by me, maybe add tests from libs?

--- Aplicatie/Helpers.py
+++ Aplicatie/Helpers.py
@@ -22,6 +22,8 @@ def generate_descriptive_stats(nums):
     stats['median'] = df['nums'].median()
     stats['skew'] = df['nums'].skew()
     stats['kurtosis'] = df['nums'].kurt()
+    stats['Kologomorov-Smirnov test vs. uniform (value should be very close to 0 for uniform):'] = KS_uniform(nums)
+    stats['Z-test vs. uniform (Value should be <-2.5 or >2.5: '] = Z_test(nums)
     return stats
 
 
@@ -37,3 +39,29 @@ def Box_Muller(u1, u2):
     z1 = np.nan_to_num(z1)
     z2 = np.nan_to_num(z2)
     return (z1.tolist(), z2.tolist())
+
+def KS_uniform(nums):
+    nums = np.clip(np.divide(nums, 255), 0.0001, 0.9999)
+    D_plus = 0
+    D_minus = 0
+    N = len(nums)
+    memory_cdf = np.sort(nums)
+    arange1 = np.arange(1.0, N + 1) / N
+    arange2 = np.arange(0.0, N) / N
+    D_plus = (arange1 - memory_cdf).max()
+    D_minus = (memory_cdf - arange2).max()
+    return(max(D_plus, D_minus))
+
+
+def Z_test(nums):
+    unique_numbers = list(set(nums))
+    my_counts = {x: nums.count(x) for x in unique_numbers}
+    df_dict = {"number": my_counts.keys(), "counts": my_counts.values()} 
+    df = pd.DataFrame.from_dict(df_dict)
+    dist_uniform = np.random.randint(df['counts'].min(), df['counts'].max(), len(df))
+    numbers_dist_uniform = range(0, len(dist_uniform))
+    dict_dist_uniform = {numbers_dist_uniform[i]: dist_uniform[i] for i in range(len(dist_uniform))}
+    data_dist_uniform = {"number": dict_dist_uniform.keys(), "counts": dict_dist_uniform.values()}
+    df2 = pd.DataFrame.from_dict(data_dist_uniform)
+    z_calc = (df['counts'].mean() - df2['counts'].mean()) / (math.sqrt(df.var()['counts'] / len(df['counts']) + df2.var()['counts'] / len(df2['counts'])))
+    return z_calc
